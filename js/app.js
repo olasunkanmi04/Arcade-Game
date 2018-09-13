038bbb5c887e74cf24aa5d8ab1906e48f48004f6
olasunkanmi04 <30435543+olasunkanmi04@users.noreply.github.com>
2018-09-13 18:52:14 +0100

    Added Score, level and Modal
    Modal to congratulate when the player gets to level 12.

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ class Enemy {
         // The image/sprite for our enemies, this uses
         // a helper we've provided to easily load images
         this.sprite = 'images/enemy-bug.png';
-    };
+    }
     // Update the enemy's position, required method for game
     // Parameter: dt, a time delta between ticks
     update(dt) {
@@ -22,50 +22,76 @@ class Enemy {
             this.speed = 100 + Math.floor(Math.random() * 222);
         }
 
+        // Check for when player collides with an enemy and return player back to starting position
         if ((player.x < this.x + 70 && player.x + 70 > this.x && player.y < this.y + 70 && player.y + 70 > this.y)) {
             setTimeout(function () {
                 player.restart();
+
             }, 200);
+            // Reduce the score when player collides with an Enemy
+            if (player.score > 0 && player.level > 1 || player.level === 12) {
+                player.score -= 5;
+                player.level -= 1;
+            };
         }
+
     }
     // Draw the enemy on the screen, required method for game
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+
     }
+
 }
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-
-
 class Player {
     constructor(x, y) {
         this.x = x;
         this.y = y;
         this.sprite = 'images/char-horn-girl.png';
-    };
+        this.score = 0;
+        this.level = 1;
+    }
     update() {
-        // checks if player has gotten to the river and if so return to it's starting position
-        if (this.y <= 0) {
-            var gamer = this;
-            setTimeout(function () {
-                // document.querySelector('canvas').style.animation = 'jello-diagonal-2 0.8s both';
-                gamer.restart();
+        // Shows the score and level on the page
+        document.querySelector('.level').innerHTML = "Level: " + this.level;
+        document.querySelector('.score').innerHTML = "Score: " + this.score;
 
-            }, 1000);
-
+        // Call modal when player gets to level 12
+        if (player.level === 12) {
+            document.querySelector('.modal').style.display = 'block';
+            document.querySelector('.modal-score').innerHTML = `You made it to Level: ${player.level} with score ${player.score}`;
         }
-    };
+        // Close modal and reset score and level
+        document.querySelector('.modal-close').addEventListener('click', function () {
+            player.level = 1;
+            player.score = 0;
+            document.querySelector('.modal').style.display = "none";
+        });
+        document.querySelector('.modal-play-again').addEventListener('click', function () {
+            player.level = 1;
+            player.score = 0;
+            document.querySelector('.modal').style.display = "none";
+        });
 
+    }
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-    };
+    }
     // Returns player to starting position
     restart() {
         this.x = 202;
         this.y = 405;
-    };
+    }
     // Handles players movement and prevents player from going outside the canvas
+    levels() {
+        if (player.score = 10) {
+            player.level += 1;
+
+        }
+    }
     handleInput(keyPress) {
         switch (keyPress) {
             case 'right':
@@ -88,9 +114,23 @@ class Player {
                     this.y += 83;
                 }
         }
-    };
+        // checks if player has gotten to the river and if so return to it's starting position and score player
+        if (this.y <= 0) {
+            let play = this;
+            setTimeout(function () {
+                play.restart();
+                play.score += 5;
+                play.level += 1;
+
+            }, 100);
+
+        }
+
+
+    }
 
 }
+
 
 
 
