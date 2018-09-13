6c782b93b45b058bac22ee34e20eb44709823ac7
olasunkanmi04 <30435543+olasunkanmi04@users.noreply.github.com>
2018-09-13 13:46:08 +0100

    Created restart function
    To set the player back to it's starting position when it reaches the river or get hit by an enemy

--- js/app.js
+++ js/app.js
@@ -21,6 +21,12 @@ class Enemy {
             this.x = -90;
             this.speed = 100 + Math.floor(Math.random() * 222);
         }
+
+        if ((player.x < this.x + 70 && player.x + 70 > this.x && player.y < this.y + 70 && player.y + 70 > this.y)) {
+            setTimeout(function () {
+                player.restart();
+            }, 200);
+        }
     }
     // Draw the enemy on the screen, required method for game
     render() {
@@ -30,17 +36,36 @@ class Enemy {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+
+
 class Player {
     constructor(x, y) {
         this.x = x;
         this.y = y;
         this.sprite = 'images/char-horn-girl.png';
     };
-    update() {}
+    update() {
+        // checks if player has gotten to the river and if so return to it's starting position
+        if (this.y <= 0) {
+            var gamer = this;
+            setTimeout(function () {
+                // document.querySelector('canvas').style.animation = 'jello-diagonal-2 0.8s both';
+                gamer.restart();
+
+            }, 1000);
+
+        }
+    };
 
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     };
+    // Returns player to starting position
+    restart() {
+        this.x = 202;
+        this.y = 405;
+    };
+    // Handles players movement and prevents player from going outside the canvas
     handleInput(keyPress) {
         switch (keyPress) {
             case 'right':
@@ -50,19 +75,23 @@ class Player {
                 break;
             case 'left':
                 if (this.x > 50) {
-                    this.x -= 102;
+                    this.x -= 100;
                 }
                 break;
             case 'up':
                 if (this.y > 0) {
-                    this.y -= 83
+                    this.y -= 83;
                 }
                 break;
             case 'down':
-                if (this.y)
+                if (this.y < 380) {
+                    this.y += 83;
+                }
         }
     };
+
 }
+
 
 
 // Now instantiate your objects.
@@ -75,7 +104,8 @@ allEnemies = [
     new Enemy(-10, 150, 500),
 ];
 // Place the player object in a variable called player
-const player = new Player(202, 405);
+let player = new Player(202, 405);
+
 
 
 // This listens for key presses and sends the keys to your
